6b6c3cf6059d9c514f0fdfff1ff3e206ce737376
Shivali-Singireddy <shivali5@mit.edu>
2025-07-13 23:37:27 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -217,7 +217,7 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         
-        <NavItem href="">Home</NavItem>
+        <NavItem href="/">Home</NavItem>
         <DropdownNavItem
   label="Continents"
   items={[
