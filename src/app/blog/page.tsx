5dc2fc15be6c022c0839877801f354594d85b754
Shivali-Singireddy <shivali5@mit.edu>
2025-07-13 19:00:21 +0100

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -35,7 +35,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
 }
 
 export const metadata: Metadata = {
-  title: 'Europe',
+  title: 'Personal Blog',
   description:
     'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
 }
