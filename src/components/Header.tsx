4ca03d42b09b04532ee8133993123e3646b913bc
Shivali-Singireddy <shivali5@mit.edu>
2025-07-13 23:41:17 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -180,11 +180,12 @@ export function DropdownNavItem({ label, items }: DropdownNavItemProps) {
       onMouseEnter={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
     >
-      <button
+      <Link
+        href="/continents"
         className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
       >
         {label}
-      </button>
+      </Link>
       <Transition
         show={isOpen}
         enter="transition ease-out duration-100"
@@ -215,19 +216,20 @@ export function DropdownNavItem({ label, items }: DropdownNavItemProps) {
 function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex rounded-full bg-white/90 px-6 py-3 text-base font-semibold text-zinc-800 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         
         <NavItem href="/">Home</NavItem>
         <DropdownNavItem
-  label="Continents"
-  items={[
-    { label: 'Europe', href: '/continents/europe' },
-    { label: 'North America', href: '/continents/north_america' },
-    { label: 'South America', href: '/continents/south_america' },
-    { label: 'Africa', href: '/continents/africa' },
-    { label: 'Asia', href: '/continents/asia' },
-  ]}
-/>
+            label="Continents"
+            href="/continents"
+            items={[
+              { label: 'Europe', href: '/continents/europe' },
+              { label: 'North America', href: '/continents/north_america' },
+              { label: 'South America', href: '/continents/south_america' },
+              { label: 'Africa', href: '/continents/africa' },
+              { label: 'Asia', href: '/continents/asia' },
+            ]}
+          />
         <NavItem href="/blog">Personal Blog</NavItem>
         <NavItem href="/about">About</NavItem>
       </ul>
