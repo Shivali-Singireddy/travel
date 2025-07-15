edf15374322ca47e870cda89a929dbf68b0b0d6e
Shivali-Singireddy <shivali5@mit.edu>
2025-07-15 15:53:38 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -420,7 +420,7 @@ export function Header() {
           <div className="flex justify-center py-2">
             <div className="relative w-[180px] sm:w-[220px] md:w-[260px] h-auto">
               <Image
-                src="/images/heading.png"
+                src="@/images/heading.png"
                 alt="Shivali logo"
                 layout="responsive"
                 width={260}
