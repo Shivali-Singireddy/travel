6b36db066c8519cc8e255b28f98a5b8c4af5696a
Shivali-Singireddy <shivali5@mit.edu>
2025-07-15 16:08:23 +0100

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,7 @@ import {
   LinkedInIcon,
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
+import headerImage from '@/images/heading.jpg'
 
 function SocialLink({
   className,
@@ -58,7 +59,7 @@ export default function About() {
         <div className="lg:pl-20">
           <div className="max-w-xs px-2.5 lg:px-0">
             <Image
-              src={portraitImage}
+              src={headerImage}
               alt=""
               sizes="(min-width: 1024px) 32rem, 20rem"
               className="w-full h-auto rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
