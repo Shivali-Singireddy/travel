b592f3c01a3e33a921efc2629471f2f684a91aa1
Shivali-Singireddy <shivali5@mit.edu>
2025-07-16 22:14:12 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ function NavLink({
       href={href}
       className="transition hover:text-teal-500 dark:hover:text-teal-400"
     >
-      {children
+      {children}
     </Link>
   )
 }
@@ -26,14 +26,21 @@ export function Footer() {
         <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 md:flex-row">
+              {/* Favicon Image */}
+              <img 
+                src="images/favicon.ico" 
+                alt="Favicon" 
+                className="mb-4 w-6 h-6 md:mb-0 md:mr-6" 
+              />
+
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/">Home</NavLink>
-                <NavLink href="/continents">Destinations</NavLink>
+                <NavLink href="/continents">Continents</NavLink>
                 <NavLink href="/blog">Personal Blog</NavLink>
                 <NavLink href="/about">About</NavLink>                        
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Weekend with Shivali. All rights
+                &copy; {new Date().getFullYear()} Shivali Singireddy. All rights
                 reserved.
               </p>
             </div>
