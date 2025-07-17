e2fd2b6f7ced85f3a8f9ee89ffa07e1761744c8c
Shivali-Singireddy <shivali5@mit.edu>
2025-07-17 10:32:16 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -180,36 +180,6 @@ export default async function Home() {
         {/* Right Purple Rectangle */}
         <div className="absolute top-0 left-1/2 h-full w-1/2 bg-[#7A5E8A]" />
 
-        {/* Center Image with Offset Border */}
-        <div
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          style={{
-            width: '320px',
-            height: '220px',
-            zIndex: 5,
-          }}
-        >
-          {/* Image itself */}
-          <img
-            src= amazon // Update path if needed
-            alt="Amazon"
-            className="w-full h-full object-cover"
-            style={{ zIndex: 5, position: 'relative' }}
-          />
-        
-          {/* Offset Border */}
-          <div
-            className="absolute top-0 left-0 border border-gray-800 pointer-events-none"
-            style={{
-              width: '320px',
-              height: '220px',
-              transform: 'translate(-8px, -8px)',
-              backgroundColor: 'transparent',
-              zIndex: 4,
-            }}
-          />
-        </div>
-
         {/* Tape Image */}    
         <Image
           src={tape}
@@ -261,8 +231,6 @@ export default async function Home() {
           />
         </div>
       </div>
-
-
 
       <Photos />
       <Container className="mt-24 md:mt-28">
