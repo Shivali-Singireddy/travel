996392aa53ef3b02d5ebb2eb7aa484a941e3c8fe
Shivali-Singireddy <shivali5@mit.edu>
2025-07-17 22:49:03 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -138,50 +138,6 @@ function Newsletter() {
   )
 }
 
-export function RotatingImageWithFade({
-  images,
-  interval = 3000, // 3 seconds
-  width = 300,
-  height = 300,
-}: {
-  images: StaticImageData[]
-  interval?: number
-  width?: number
-  height?: number
-}) {
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentIndex((prev) => (prev + 1) % images.length)
-    }, interval)
-    return () => clearInterval(timer)
-  }, [images.length, interval])
-
-  return (
-    <div
-      className="relative"
-      style={{
-        width,
-        height,
-      }}
-    >
-      {images.map((img, i) => (
-        <Image
-          key={img.src}
-          src={img}
-          alt={`Rotating Image ${i}`}
-          width={width}
-          height={height}
-          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out ${
-            i === currentIndex ? 'opacity-100 z-10' : 'opacity-0 z-0'
-          }`}
-        />
-      ))}
-    </div>
-  )
-}
-
 function Photos() {
   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
