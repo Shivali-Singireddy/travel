fb6b498e4e6ffd136ae05d0ee3e10b3f0e0d4cb8
Shivali-Singireddy <shivali5@mit.edu>
2025-07-17 19:34:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -224,13 +224,13 @@ export default async function Home() {
         {/* Left Purple Box with Text */}
         <div
           className="absolute top-1/2 left-[5%] -translate-y-1/2 z-20"
-          style={{ width: '480px', height: '480px' }}
+          style={{ width: '480px', height: '600px' }} // increased height here
         >
           <div
             className="absolute top-0 left-0 bg-[#7A5E8A] p-6"
             style={{
               width: '480px',
-              height: '480px',
+              height: '600px', // increased height here
               transform: 'rotate(-5deg)',
             }}
           >
@@ -256,7 +256,7 @@ export default async function Home() {
             className="absolute top-0 left-0 border border-black pointer-events-none"
             style={{
               width: '480px',
-              height: '480px',
+              height: '600px', // increased height here
               transform: 'rotate(-5deg) translate(8px, 8px)',
               backgroundColor: 'transparent',
               zIndex: 30,
@@ -264,6 +264,7 @@ export default async function Home() {
           />
         </div>
       </div>
+
     </>
   )
 }
