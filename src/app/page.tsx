4a61c863dedfe5cd3d47f7fa0cdb651c1c59f902
Shivali-Singireddy <shivali5@mit.edu>
2025-07-17 23:07:43 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -179,17 +179,17 @@ export default async function Home() {
           style={{
             position: 'relative',
             width: 470,       // outline container bigger than image container
-            height: 300,
+            height: 400,
           }}
         >
           {/* Outline */}
           <div
             style={{
               position: 'absolute',
-              top: -10,        // offset upwards
-              left: -10,       // offset leftwards
+              top: 30,        // offset upwards
+              right: -10,       // offset leftwards
               width: 490,      // slightly bigger width than container (470 + 20 offset total)
-              height: 320,     // slightly bigger height (300 + 20)
+              height: 400,     // slightly bigger height (300 + 20)
               border: '1px solid black',
               borderRadius: 0,
               pointerEvents: 'none',  // don't block mouse events
