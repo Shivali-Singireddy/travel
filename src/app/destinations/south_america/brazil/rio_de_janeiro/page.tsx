08d561ae7950067e7fd3dce6f48fe4ea2fbc12ae
Shivali-Singireddy <shivali5@mit.edu>
2025-07-18 23:50:50 +0100

    Update page.tsx

--- src/app/destinations/south_america/brazil/rio_de_janeiro/page.tsx
+++ src/app/destinations/south_america/brazil/rio_de_janeiro/page.tsx
@@ -1,1 +1,28 @@
+import Image from 'next/image'
+import rio_post from '@/app/destinations/south_america/brazil/rio_de_janeiro/rio_post.png'
 
+export default function RioPostPage() {
+  return (
+    <div className="relative w-full min-h-screen bg-[#FAF5EF] border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
+      <div className="relative w-full" style={{ aspectRatio: '3 / 4', maxWidth: 600 }}>
+        {/* Outline around image */}
+        <div
+          className="absolute top-[-10px] left-[-10px] border border-black pointer-events-none"
+          style={{
+            width: 'calc(100% + 20px)',
+            height: 'calc(100% + 20px)',
+          }}
+        />
+
+        {/* Image itself */}
+        <Image
+          src={rio_post}
+          alt="Rio Post"
+          fill
+          style={{ objectFit: 'cover' }}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
