66ed07f0c551a62176fade0e80c37ad20526be87
Shivali-Singireddy <shivali5@mit.edu>
2025-07-18 15:01:38 +0100

    Update page.tsx

--- src/app/destinations/page.tsx
+++ src/app/destinations/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Destinations() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Destinations</h1>
+      <p>Welcome to the Destination travel page.</p>
+    </div>
+  )
+}
