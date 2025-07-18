966e161207ba583ee268e4c1a3a4ce5210a51f0f
Shivali-Singireddy <shivali5@mit.edu>
2025-07-18 23:52:11 +0100

    Update page.tsx

--- src/app/destinations/south_america/brazil/sao_paolo/page.tsx
+++ src/app/destinations/south_america/brazil/sao_paolo/page.tsx
@@ -1,1 +1,28 @@
+import Image from 'next/image'
+import sao_paolo_post from '@/app/destinations/south_america/brazil/sao_paolo/sao_paolo_post.png'
 
+export default function SaoPaoloPostPage() {
+  return (
+    <div className="relative w-full min-h-screen bg-[#FAF5EF] border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
+      <div className="relative w-full" style={{ aspectRatio: '3 / 4', maxWidth: 600 }}>
+        {/* Outline around image */}
+        <div
+          className="absolute top-[-10px] left-[-10px] border border-black pointer-events-none"
+          style={{
+            width: 'calc(100% + 20px)',
+            height: 'calc(100% + 20px)',
+          }}
+        />
+
+        {/* Image itself */}
+        <Image
+          src={sao_paolo_post}
+          alt="Sao Paolo Post"
+          fill
+          style={{ objectFit: 'cover' }}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
