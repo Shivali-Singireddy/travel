df5350a8a53836141faa2b5d1b2a837b4146fe99
Shivali-Singireddy <shivali5@mit.edu>
2025-07-19 23:05:03 +0100

    Update page.tsx

--- src/app/destinations/north_america/california/page.tsx
+++ src/app/destinations/north_america/california/page.tsx
@@ -14,7 +14,7 @@ export default function CaliforniaPage() {
 
   return (
     <div className="w-full min-h-screen bg-white px-6 py-10 max-w-screen-xl mx-auto">
-      <h1 className="text-3xl font-bold mb-8 text-center text-purple-900">Explore Hawaii</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center text-purple-900">Explore California</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {posts.map((post) => (
