e85e1439f3d1772bd8102742b5471249a89b9a2a
Shivali-Singireddy <shivali5@mit.edu>
2025-07-21 15:57:54 +0100

    Create page.tsx

---
+++ src/app/destinations/asia/turkey/cappadocia/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import cappadocia_post from '@/app/destinations/asia/turkey/cappadocia/cappadocia_post.png'
+
+export default function CappadociaPostPage() {
+  return (
+    <div className="relative w-full min-h-screen bg-white border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
+      <div className="w-full max-w-[1000px] border border-gray-300 overflow-hidden">
+        <Image
+          src={cappadocia_post}
+          alt="Cappadocia Post"
+          width={1000}           // specify natural width for optimization
+          style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
