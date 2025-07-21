0e3b4b38f0e478f32f02f22278dcd30929be95fb
Shivali-Singireddy <shivali5@mit.edu>
2025-07-21 16:11:10 +0100

    Create page.tsx

---
+++ src/app/destinations/asia/turkey/istanbul/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import istanbul_post from '@/app/destinations/asia/turkey/istanbul/istanbul_post.png'
+
+export default function IstanbulPostPage() {
+  return (
+    <div className="relative w-full min-h-screen bg-white border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
+      <div className="w-full max-w-[1000px] border border-gray-300 overflow-hidden">
+        <Image
+          src={istanbul_post}
+          alt="Istanbul Post"
+          width={1000}           // specify natural width for optimization
+          style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
