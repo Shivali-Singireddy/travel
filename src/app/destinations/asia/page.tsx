bdabb90b36ec26b28d9948de62c328723152c598
Shivali-Singireddy <shivali5@mit.edu>
2025-07-21 12:11:59 +0100

    Create page.tsx

---
+++ src/app/destinations/asia/page.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import turkey from '@/app/destinations/asia/turkey.png'
+
+
+export default function SouthAmericaPage() {
+  const posts = [
+    {
+      title: 'Turkey',
+      slug: 'turkey',
+      image: turkey,
+    },
+  ]
+
+  return (
+    <div className="w-full min-h-screen bg-white px-6 py-10 max-w-screen-xl mx-auto">
+      <h1 className="text-3xl font-bold mb-8 text-center text-purple-900">Explore Asia</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {posts.map((post) => (
+          <Link
+            key={post.slug}
+            href={`/destinations/asia/${post.slug}`}
+            className="block border border-gray-300 overflow-hidden hover:shadow-lg transition"
+          >
+            <div className="relative w-full h-75">
+              <Image
+                src={post.image}
+                alt={post.title}
+                fill
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
