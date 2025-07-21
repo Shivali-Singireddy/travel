20e7d66585eb83f859b80b608ae446490c743803
Shivali-Singireddy <shivali5@mit.edu>
2025-07-21 21:35:26 +0100

    Update page.tsx

--- src/app/destinations/africa/morocco/chefchaouen/page.tsx
+++ src/app/destinations/africa/morocco/chefchaouen/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
-import paris_post from '@/app/destinations/europe/paris/paris_post.png'
+import chefchaouen_post from '@/app/destinations/africa/morocco/chefchaouen/chef_post.png'
 
-export default function ParisPostPage() {
+export default function ChefchaouenPostPage() {
   return (
     <div className="relative w-full min-h-screen bg-white border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
       <div className="w-full max-w-[1000px] border border-gray-300 overflow-hidden">
         <Image
-          src={paris_post}
-          alt="Paris Post"
+          src={chefchaouen_post}
+          alt="Chefchaouen Post"
           width={1000}           // specify natural width for optimization
           style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
           priority
