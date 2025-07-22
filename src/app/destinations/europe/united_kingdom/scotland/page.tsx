bf04abc1bd4712629dcc3f9ea41c167d8c918486
Shivali-Singireddy <shivali5@mit.edu>
2025-07-22 10:43:28 +0100

    Create page.tsx

---
+++ src/app/destinations/europe/united_kingdom/scotland/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import scotland_post from '@/app/destinations/europe/united_kingdom/scotland/scotland_post.png'
+
+export default function ScotlandPostPage() {
+  return (
+    <div className="relative w-full min-h-screen bg-white border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
+      <div className="w-full max-w-[1000px] border border-gray-300 overflow-hidden">
+        <Image
+          src={scotland_post}
+          alt="Scotland Post"
+          width={1000}           // specify natural width for optimization
+          style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
