4506ca58aadc110b91bbe3a2bc9ba3bbb7db7f8e
Shivali-Singireddy <shivali5@mit.edu>
2025-07-22 15:23:46 +0100

    Create page.tsx

---
+++ src/app/destinations/europe/amsterdam/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import amsterdam_post from '@/app/destinations/europe/amsterdam/amsterdam_post.png'
+
+export default function AmsterdamPostPage() {
+  return (
+    <div className="relative w-full min-h-screen bg-white border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
+      <div className="w-full max-w-[1000px] border border-gray-300 overflow-hidden">
+        <Image
+          src={amsterdam_post}
+          alt="Amsterdam Post"
+          width={1000}           // specify natural width for optimization
+          style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
+          priority
+        />
+      </div>
+    </div>
+  )
+}
