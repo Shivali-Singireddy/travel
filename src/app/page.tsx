cc0161d4593ab0f4164ad9188b5c3f7fa6370225
Shivali-Singireddy <shivali5@mit.edu>
2025-07-23 15:39:41 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -319,6 +319,7 @@ export default async function Home() {
             ))}
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
+            <ContinentCarousel />
             <Newsletter />
           </div>
         </div>
