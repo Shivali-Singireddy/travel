03c685e9017014af2fb8c23ecbbb2ba19760868a
Shivali-Singireddy <shivali5@mit.edu>
2025-07-23 14:53:27 +0100

    Update page.tsx

--- src/app/destinations/europe/italy/rome/page.tsx
+++ src/app/destinations/europe/italy/rome/page.tsx
@@ -6,7 +6,7 @@ export default function RomePostPage() {
     <div className="relative w-full min-h-screen bg-white border border-[#e0e0e0] mx-auto flex flex-col items-center px-4 py-8 max-w-screen-xl">
       <div className="w-full max-w-[1000px] border border-gray-300 overflow-hidden">
         <Image
-          src={Rome_post}
+          src={rome_post}
           alt="Rome Post"
           width={1000}           // specify natural width for optimization
           style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
