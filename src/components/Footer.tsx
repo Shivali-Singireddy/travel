3ee48170f65e26b03688370178a701a619743f5c
Shivali-Singireddy <shivali5@mit.edu>
2025-07-23 15:42:04 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import favicon from '@/images/favicon.jpg'
+import Image from 'next/image'
+
 
 import { ContainerInner, ContainerOuter } from '@/components/Container'
 
