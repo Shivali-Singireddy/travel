453aa40732a945d8aa2feaca9a43daa4ed00627d
Shivali-Singireddy <shivali5@mit.edu>
2025-07-25 13:01:44 +0100

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <Container className="mt-16 sm:mt-32">
+    <Container className="mt-6 sm:mt-10">
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
         <div className="lg:pl-20">
           <div className="max-w-xs px-2.5 lg:px-0">
@@ -66,7 +66,7 @@ export default function About() {
           </div>
         </div>
         <div className="lg:order-first lg:row-span-2">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+          <h1 className="text-4xl font-bold tracking-tight text-purple-800 sm:text-5xl dark:text-purple-300">
             Hi it's Shivali! Welcome to my personal blog :).
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
