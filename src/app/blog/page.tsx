cedbd9e78364c4e2a7f30b3630452ae270949b87
Shivali-Singireddy <shivali5@mit.edu>
2025-07-30 00:07:50 +0100

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -57,18 +57,13 @@ export default async function BlogIndex() {
           style={{ objectFit: 'cover' }}
         />
       </div>
-      <SimpleLayout
-        title="Taking life one weekend at a time."
-        intro="All of my non travel related stories"
-      >
-        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-          <div className="flex max-w-3xl flex-col space-y-16">
-            {articles.map((article) => (
-              <Article key={article.slug} article={article} />
-            ))}
-          </div>
+      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="flex max-w-3xl flex-col space-y-16">
+          {articles.map((article) => (
+            <Article key={article.slug} article={article} />
+          ))}
         </div>
-      </SimpleLayout>
+      </div>
     </div>
   )
 }
