47280fd888781a60078a46d88c686ee29c25d90f
Shivali-Singireddy <shivali5@mit.edu>
2025-07-29 17:05:28 +0100

    Update page.tsx

--- src/app/destinations/north_america/page.tsx
+++ src/app/destinations/north_america/page.tsx
@@ -5,6 +5,12 @@ import hawaii from '@/app/destinations/north_america/hawaii.png'
 import mexico from '@/app/destinations/north_america/mexico.png'
 import north_america from '@/app/destinations/north_america/north_america_top.png'
 import top_regions from '@/app/destinations/top_regions.png'
+import maui_post from '@/app/destinations/north_america/hawaii/maui/maui_post.png'
+import kauai_post from '@/app/destinations/north_america/hawaii/kauai/kauai_post.png'
+import kalalau_post from '@/app/destinations/north_america/hawaii/kauai/kalalau_trail/kalalau_post.png'
+import big_island_post from '@/app/destinations/north_america/hawaii/big_island/big_island_post.png'
+import cancun_post from '@/app/destinations/north_america/mexico/cancun/cancun_post.png'
+import valladolid_post from '@/app/destinations/north_america/mexico/valladolid/valladolid_post.png'
 
 
 
@@ -24,6 +30,45 @@ export default function NorthAmericaPage() {
       title: 'California',
       slug: 'california',
       image: california,
+    },
+  ]
+
+  const destinations = [
+    {
+      title: 'Kalalau Trail',
+      slug: 'hawaii/kauai/kalalau_trail',
+      image: kalalau_post,
+      description: '4 day itinerary covering Road to Hana, Haleakala, and Waihee Ridge.',
+    },
+    {
+      title: 'Maui',
+      slug: 'hawaii/maui',
+      image: maui_post,
+      description: '4 Day Itinerary in Maui: Hostels, Hikes & Haleakalā Sunsets',
+    },
+    {
+      title: 'Cancun',
+      slug: 'mexico/cancun',
+      image: cancun_post,
+      description: '4 day itinerary covering Road to Hana, Haleakala, and Waihee Ridge.',
+    },
+    {
+      title: 'Kauai',
+      slug: 'hawaii/kauai',
+      image: kauai_post,
+      description: '4 day itinerary covering Road to Hana, Haleakala, and Waihee Ridge.',
+    },
+    {
+      title: 'Big Island',
+      slug: 'hawaii/big_island',
+      image: big_island_post,
+      description: '4 day itinerary covering Road to Hana, Haleakala, and Waihee Ridge.',
+    },
+    {
+      title: 'Valladolid/Tulum',
+      slug: 'mexico/valladolid',
+      image: valladolid_post,
+      description: '4 day itinerary covering Road to Hana, Haleakala, and Waihee Ridge.',
     },
   ]
 
@@ -69,6 +114,42 @@ export default function NorthAmericaPage() {
                 fill
                 style={{ objectFit: 'cover' }}
               />
+            </div>
+          </Link>
+        ))}
+      </div>
+       <div className="w-full flex flex-col items-center mt-12 mb-6">
+        <div className="relative w-full flex justify-center my-20">
+          <div className="w-full">
+            <hr className="border-t-2 border-[#7A5E8A] opacity-90" />
+          </div>
+          <Image
+            src={feat_dest}
+            alt="Featured Destinations"
+            width={700}
+            height={200}
+            className="absolute top-1/2 transform -translate-y-1/2 z-10"
+          />
+        </div>      
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {destinations.map((dest) => (
+          <Link
+            key={dest.slug}
+            href={`/destinations/north_america/${dest.slug}`}
+            className="block border border-gray-300 rounded-lg overflow-hidden hover:shadow-lg transition"
+          >
+            <div className="relative w-full h-64">
+              <Image
+                src={dest.image}
+                alt={dest.title}
+                fill
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+            <div className="p-4 bg-white text-center">
+              <h2 className="text-xl font-semibold text-purple-800 mb-2">{dest.title}</h2>
+              <p className="text-gray-700 text-sm">{dest.description}</p>
             </div>
           </Link>
         ))}
