71d77d986f37ba9875e4fc2b005f34ffb4898fe5
Shivali-Singireddy <shivali5@mit.edu>
2025-08-01 23:49:52 +0100

    Update page.tsx

--- src/app/destinations/north_america/hawaii/maui/page.tsx
+++ src/app/destinations/north_america/hawaii/maui/page.tsx
@@ -34,10 +34,28 @@ export default function MauiPostPage() {
 
           <h3 className="text-xl font-semibold mb-2">Why Aloha Surf Hostel Was the Perfect Base for Our Trip</h3>
           <p className="text-base text-gray-800 leading-relaxed">
-            We stayed at the Aloha Surf Hostel for this trip...
+            We stayed at the Aloha Surf Hostel for this trip, and it felt worth it to me. 
+            The price was around $70 per night including tax. 
+            Accommodation and food across the Hawaiian islands tend to be pricey, which isn’t surprising given that Hawaii is a remote island chain that relies heavily on imported goods. 
+            Still, the experience made it a great value for what we got.
           </p>
-          {/* Include other accommodation-related paragraphs here */}
-
+          <p className="text-base text-gray-800 dark:text-gray-200 leading-relaxed mt-2">
+            However, what attracts me to a hostel for this island was the built-in “free” organized tours they run with the hostel. 
+            I put the “free” in quotations because it is recommended that you tip ~$20-$30 to your guide. 
+            However, considering that guided tours on sites such as GetYourGuide and Viator are around $100, this is still a steal.
+          </p>
+          <p className="text-base text-gray-800 dark:text-gray-200 leading-relaxed mt-2">
+            Another reason I chose to stay at a hostel for this trip was the sense of community. 
+            I really enjoyed connecting with other travelers, whether it was in my room or during the organized hostel tours. 
+            After the day’s adventures, there was always a group of friendly, like-minded people hanging out in the common areas, eager to explore more of the island or just relax together. 
+            It was easy to find someone up for joining in on any activity, which made the whole experience even more memorable.
+          </p>
+          <p className="text-base text-gray-800 dark:text-gray-200 leading-relaxed mt-2">
+            With Aloha Surf in particular, there was a hot tub to relax our muscles in after a long hike and a cozy firepit for s’mores, which I loved. 
+            Plus, every morning they had ingredients to make a free pancake breakfast with coffee. 
+            Here’s my confession for this post: I actually learned to make chocolate chip pancakes for the first time (please don’t judge me, I’m a little spoiled)
+          </p>
+          
           {/* Subsection: Hostel Tours */}
           <div className="mt-6">
             <h3 className="text-xl font-semibold mb-2">How the Hostel Tours Work</h3>
